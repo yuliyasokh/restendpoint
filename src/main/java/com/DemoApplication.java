6f0e82c524ab8cf8587a20f6511b590fff008e9f
Iuliia Sokhranna <yuliya.sokh@gmail.com>
2019-09-30 20:37:07 +0300

    war package

--- src/main/java/com/DemoApplication.java
+++ src/main/java/com/DemoApplication.java
@@ -2,11 +2,26 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoApplication {
+@RestController
+public class DemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Hello World from Tomcat";
     }
 }
 
