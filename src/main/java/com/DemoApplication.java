246537bbb1771660af0a1f53f6a4746e5810ed65
Iuliia Sokhranna <yuliya.sokh@gmail.com>
2019-10-18 23:26:04 +0300

    added property file

--- src/main/java/com/DemoApplication.java
+++ src/main/java/com/DemoApplication.java
@@ -1,27 +1,43 @@
 package com;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
-@RestController
-public class DemoApplication extends SpringBootServletInitializer {
+public class DemoApplication implements ApplicationRunner {
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(DemoApplication.class);
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
+
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-    @RequestMapping(value = "/")
-    public String hello() {
-        return "Hello World from Tomcat";
+
+    @Override
+    public void run(ApplicationArguments arg0) throws Exception {
+        logger.info("this is a info message");
+        logger.warn("this is a warn message");
+        logger.error("this is a error message");
+        System.out.println("Hello World from Application Runner");
     }
 }
 
