9ffa6dd092c115c310ac9deaa0f6156a1b701e03
Iuliia Sokhranna <yuliya.sokh@gmail.com>
2019-11-03 20:12:59 +0200

    restfull web service

---
+++ src/main/java/com/ProductServiceController.java
@@ -0,0 +1,30 @@
+package com;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ProductServiceController {
+
+    private static Map<String, Product> productRepo = new HashMap<>();
+    static {
+        Product honey = new Product();
+        honey.setId("1");
+        honey.setName("Honey");
+        productRepo.put(honey.getId(), honey);
+
+        Product almond = new Product();
+        almond.setId("2");
+        almond.setName("Almond");
+        productRepo.put(almond.getId(), almond);
+    }
+
+    @RequestMapping(value = "/products")
+    public ResponseEntity<Object> getProduct() {
+        return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+    }
+}
