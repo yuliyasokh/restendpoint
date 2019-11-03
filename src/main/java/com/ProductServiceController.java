0ccdf0a5092ca7138d0dc294fad3568a962185fd
Iuliia Sokhranna <yuliya.sokh@gmail.com>
2019-11-03 20:19:24 +0200

    added post put delete

--- src/main/java/com/ProductServiceController.java
+++ src/main/java/com/ProductServiceController.java
@@ -27,4 +27,25 @@ public class ProductServiceController {
     public ResponseEntity<Object> getProduct() {
         return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
     }
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ResponseEntity<Object> createProduct(@RequestBody Product product) {
+        productRepo.put(product.getId(), product);
+        return new ResponseEntity<>("Product is created successfully", HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product) {
+        productRepo.remove(id);
+        product.setId(id);
+        productRepo.put(id, product);
+        return new ResponseEntity<>("Product is updated successfully", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> delete(@PathVariable("id") String id) {
+        productRepo.remove(id);
+        return new ResponseEntity<>("Product is deleted successfully", HttpStatus.OK);
+    }
+
 }
