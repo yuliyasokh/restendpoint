e433810bdd86981ff5da1c970c8b1ac6ded519af
Iuliia Sokhranna <yuliya.sokh@gmail.com>
2019-11-10 15:02:37 +0200

    Added filter

--- src/main/java/com/interceptor/ProductServiceInterceptor.java
+++ src/main/java/com/interceptor/ProductServiceInterceptor.java
@@ -1,4 +1,4 @@
-package com;
+package com.interceptor;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
--- src/main/java/com/interceptor/ProductServiceInterceptorAppConfig.java
+++ src/main/java/com/interceptor/ProductServiceInterceptorAppConfig.java
@@ -1,4 +1,4 @@
-package com;
+package com.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
